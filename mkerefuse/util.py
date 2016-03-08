91a48f80cada4e557a35ba192d2d69438e169c5d
Ben Tomasik <ben@tomasik.io>
2016-03-08 02:21:09 -0600

    Add logging setup

--- mkerefuse/util.py
+++ mkerefuse/util.py
@@ -1,5 +1,15 @@
 import json
+import logging
+import logging.config
+import os.path
+import yaml
 from lxml import html
+
+
+DEFAULT_LOGGING_CONFIG = {
+    'level': logging.INFO,
+    'disable_existing_loggers': False,
+}
 
 
 class XPathObject(object):
@@ -41,3 +51,40 @@ class XPathObject(object):
             self.__dict__,
             indent=4,
             separators=(',', ': '))
+
+def setup_logging(
+        config_path="logging.yaml",
+        config_path_env="LOG_CFG_PATH"
+):
+    """Setup the python logger
+
+    Args:
+        config_path (str): Path to a default config file
+        config_path_env (str): Env variable to specify alternate logging config
+    """
+    config_override = os.getenv(config_path_env, None)
+    config_path = config_path if not config_override else config_override
+
+    if os.path.exists(config_path):
+        with open(config_path, "rt") as yaml_file:
+            config = yaml.load(yaml_file.read())
+
+        logging.config.dictConfig(config)
+        logging.debug("Setup logging from '{}'".format(config_path))
+
+    else:
+        logging.debug("Setup logging from defaults")
+        logging.basicConfig(**DEFAULT_LOGGING_CONFIG)
+
+
+class LogProducer(object):
+    """Simple class to add in class-oriented logging"""
+    def __init__(self, subname=None):
+        logger_name = ".".join([
+            self.__class__.__module__,
+            self.__class__.__name__
+        ])
+
+        if subname:
+            logger_name += " ({})".format(subname)
+        self._log = logging.getLogger(logger_name)
