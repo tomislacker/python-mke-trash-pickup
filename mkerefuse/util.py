dbfb5c425d053d950b50cb1a2e9fc0edfd88ad37
Ben Tomasik <ben@tomasik.io>
2016-03-08 02:34:06 -0600

    Ensure LogProducer is defined before XPathObject

--- mkerefuse/util.py
+++ mkerefuse/util.py
@@ -12,6 +12,44 @@ DEFAULT_LOGGING_CONFIG = {
 }
 
 
+def setup_logging(
+        config_path="logging.yaml",
+        config_path_env="LOG_CFG_PATH"
+):
+    """Setup the python logger
+
+    Args:
+        config_path (str): Path to a default config file
+        config_path_env (str): Env variable to specify alternate logging config
+    """
+    config_override = os.getenv(config_path_env, None)
+    config_path = config_path if not config_override else config_override
+
+    if os.path.exists(config_path):
+        with open(config_path, "rt") as yaml_file:
+            config = yaml.load(yaml_file.read())
+
+        logging.config.dictConfig(config)
+        logging.debug("Setup logging from '{}'".format(config_path))
+
+    else:
+        logging.debug("Setup logging from defaults")
+        logging.basicConfig(**DEFAULT_LOGGING_CONFIG)
+
+
+class LogProducer(object):
+    """Simple class to add in class-oriented logging"""
+    def __init__(self, subname=None):
+        logger_name = ".".join([
+            self.__class__.__module__,
+            self.__class__.__name__
+        ])
+
+        if subname:
+            logger_name += " ({})".format(subname)
+        self._log = logging.getLogger(logger_name)
+
+
 class XPathObject(LogProducer):
     """Helper for importing response [X]HTML into a class instance"""
 
@@ -52,40 +90,3 @@ class XPathObject(LogProducer):
             self.__dict__,
             indent=4,
             separators=(',', ': '))
-
-def setup_logging(
-        config_path="logging.yaml",
-        config_path_env="LOG_CFG_PATH"
-):
-    """Setup the python logger
-
-    Args:
-        config_path (str): Path to a default config file
-        config_path_env (str): Env variable to specify alternate logging config
-    """
-    config_override = os.getenv(config_path_env, None)
-    config_path = config_path if not config_override else config_override
-
-    if os.path.exists(config_path):
-        with open(config_path, "rt") as yaml_file:
-            config = yaml.load(yaml_file.read())
-
-        logging.config.dictConfig(config)
-        logging.debug("Setup logging from '{}'".format(config_path))
-
-    else:
-        logging.debug("Setup logging from defaults")
-        logging.basicConfig(**DEFAULT_LOGGING_CONFIG)
-
-
-class LogProducer(object):
-    """Simple class to add in class-oriented logging"""
-    def __init__(self, subname=None):
-        logger_name = ".".join([
-            self.__class__.__module__,
-            self.__class__.__name__
-        ])
-
-        if subname:
-            logger_name += " ({})".format(subname)
-        self._log = logging.getLogger(logger_name)
